f6584a820bb937091c3d45d568a505810b8ccbe4
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-12 14:56:09 +0300

    1.8.0-1

---
+++ Package.swift
@@ -0,0 +1,94 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.8.0-1"
+
+let package = Package(
+    name: "BNBEyes",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBEyes",
+            targets: ["BNBEyes"]
+        ),
+        .library(
+            name: "BNBSdkCore",
+            targets: ["BNBSdkCore"]
+        ),
+        .library(
+            name: "BNBEffectPlayer",
+            targets: ["BNBEffectPlayer"]
+        ),
+        .library(
+            name: "BNBScripting",
+            targets: ["BNBScripting"]
+        ),
+        .library(
+            name: "BNBFaceTracker",
+            targets: ["BNBFaceTracker"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBEffectPlayer.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBScripting.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBFaceTracker.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBEyes",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBEyes.zip",
+            checksum: "e64c04a3474860d158660d61be5f366592b60dd3a727dde3931f1c297236d243"
+        ),
+        .target(
+            name: "BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBEffectPlayer",
+            dependencies: [
+                .product(
+                    name: "BNBEffectPlayer",
+                    package: "BNBEffectPlayer"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBScripting",
+            dependencies: [
+                .product(
+                    name: "BNBScripting",
+                    package: "BNBScripting"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceTracker",
+            dependencies: [
+                .product(
+                    name: "BNBFaceTracker",
+                    package: "BNBFaceTracker"
+                ),
+            ]
+        ),
+    ]
+)
