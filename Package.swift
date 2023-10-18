bdc9726b23cbdd087823b4d65ef6097883be96ce
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-18 15:17:48 +0300

    1.8.0-63-g6aa1953f1a

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-63-g5825a46ccf"
+let bnbPackageVersion: Version = "1.8.0-63-g6aa1953f1a"
 
 let package = Package(
     name: "BNBEyes",
@@ -41,8 +41,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBEyes",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-63-g5825a46ccf/BNBEyes.zip",
-            checksum: "f3be30ec437d4c86d57a27642f08d065447219cf9429b44ea37fee9c08242f1a"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-63-g6aa1953f1a/BNBEyes.zip",
+            checksum: "5a50cdb4624e57a07101c0d743b97b6f83c1ba9958af5d19bdfec866a547934d"
         ),
         .target(
             name: "BNBEyes_BNBSdkCore",
